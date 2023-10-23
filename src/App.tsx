acb3013ecfc0eff9fed40f6bd073cd1b79d588e6
LucasfNeves <lucasfarias.fn@gmail.com>
2023-10-23 11:06:15 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Global.css'
+import { Header } from './components/HeaderInput/Header'
+export function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+
---
+++ src/components/HeaderInput/Header.tsx
@@ -0,0 +1,27 @@
+import styles from './Header.module.css'
+
+export function Header (){
+    return(
+        <>
+            <header className={styles.containerHeader}>
+                <h1 className={styles.title}>Central de Tarefas</h1>
+                <form id="form">
+                    <input
+                        className={styles.inputTask} 
+                        type="text" 
+                        autoComplete="off"
+                        placeholder="Adicione uma nova tarefa" id="inputTask"
+                    />
+                    <button
+                        title="Adicionar Tarefa"
+                        className={styles.buttonAddTask}
+                    > 
+                        <span className={styles.textButton}>Adicionar</span>
+                        <img src="src/assets/icon-add.svg" alt="Icon-add" />
+                    </button>
+                </form> 
+            </header>
+        </>
+    )
+}
+    
